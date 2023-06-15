3f6de29199bf1cca50464f24a1de00022eba4b25
hinken <phnome@protonmail.com>
2023-06-15 23:54:45 +0200

    working on some config files

--- src/config/window_config.rs
+++ src/config/window_config.rs
@@ -1,13 +1,23 @@
 use winit::{
+    dpi::{LogicalSize, Size},
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
-pub struct WindowConfig {}
 
-pub fn main_window() {
+pub struct WindowConfig {
+    pub resolution_x: i32,
+    pub resolution_y: i32,
+    pub window_title: String,
+}
+
+pub fn main_window(config: WindowConfig) {
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+        .with_title(config.window_title)
+        .with_inner_size(LogicalSize::new(config.resolution_x, config.resolution_y))
+        .build(&event_loop)
+        .unwrap();
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
