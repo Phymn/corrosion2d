c108ab7017bf1de2e55f189ebf33ec78533b3127
hinken <phnome@protonmail.com>
2023-06-16 23:53:02 +0200

    removing eventhandler

--- src/config/window_config.rs
+++ src/config/window_config.rs
@@ -1,9 +1,4 @@
-use winit::{
-    dpi::{LogicalSize, Size},
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+use winit::{dpi::LogicalSize, window::WindowBuilder};
 
 pub struct WindowConfig {
     pub resolution_x: i32,
@@ -11,23 +6,10 @@ pub struct WindowConfig {
     pub window_title: String,
 }
 
-pub fn main_window(config: WindowConfig) {
-    let event_loop = EventLoop::new();
+pub fn main_window(config: WindowConfig, &event_loop: winit::event_loop) {
     let window = WindowBuilder::new()
         .with_title(config.window_title)
         .with_inner_size(LogicalSize::new(config.resolution_x, config.resolution_y))
         .build(&event_loop)
         .unwrap();
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            _ => (),
-        }
-    });
 }
