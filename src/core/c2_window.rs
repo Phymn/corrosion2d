5ce9b66780f71b0ab56e49f831f29ae808e49546
hinken <phnome@protonmail.com>
2023-06-18 00:58:36 +0200

    still refactoring window handler

--- src/core/c2_window.rs
+++ src/core/c2_window.rs
@@ -1,33 +1,31 @@
-use winit::{dpi::LogicalSize, window::WindowBuilder};
+use winit::dpi::LogicalSize;
+use winit::window::WindowBuilder;
 
-pub struct C2Window {
+pub struct C2WindowConfig {
     pub(crate) width: i32,
     pub(crate) height: i32,
     pub(crate) decorations: bool,
     pub(crate) window_title: String,
 }
 
-impl Default for C2Window {
+impl Default for C2WindowConfig {
     fn default() -> Self {
         Self {
-            width: Some(1920),
-            height: Some(1080),
+            width: 1920,
+            height: 1080,
             decorations: false,
-            window_title: from::String("A Mourning Light"),
+            window_title: String::from("A Mourning Light"),
         }
     }
 }
 
-impl C2Window {
-    //
-    pub(crate) fn spawn_window(
-        config: C2WindowConfig,
-        event_loop: &winit::event_loop::EventLoop<()>,
-    ) {
-        let window = WindowBuilder::new()
-            .with_title(config.window_title)
-            .with_inner_size(LogicalSize::new(config.resolution_x, config.resolution_y))
-            .build(event_loop)
-            .unwrap();
+impl C2WindowConfig {
+    pub(crate) fn into(self) -> WindowBuilder {
+        let mut window_builder = WindowBuilder::new();
+
+        window_builder
+            .with_decorations(self.decorations)
+            .with_title(self.window_title)
+            .with_inner_size(LogicalSize::new(self.height, self.width))
     }
 }
