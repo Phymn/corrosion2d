4d48dab31a87bb9d12333c43c0a1fbf3bcb958f9
hinken <phnome@protonmail.com>
2023-06-29 01:47:02 +0200

    refactoring to KISS

---
+++ src/C2D_Window.rs
@@ -0,0 +1,4 @@
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::window::{Window, WindowBuilder};
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+mod C2D_Window;
 pub mod c2_resources;
 pub(crate) mod core;
