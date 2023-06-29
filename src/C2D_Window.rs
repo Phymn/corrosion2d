fcb762a5d28aebe42e653cbefe6e60ced4aba708
hinken <phnome@protonmail.com>
2023-06-30 00:09:25 +0200

    at least its honest work

--- src/C2D_Window.rs
+++ src/C2D_Window.rs
@@ -2,3 +2,5 @@ use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::window::{Window, WindowBuilder};
+
+pub struct C2DWindowBuilder {}
