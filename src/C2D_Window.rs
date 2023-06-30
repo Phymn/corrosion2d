def9dca8c8223fa4e0ccdf3948ab913f25212cf8
hinken <phnome@protonmail.com>
2023-06-30 23:56:02 +0200

    adding structs

--- src/C2D_Window.rs
+++ src/C2D_Window.rs
@@ -3,4 +3,13 @@ use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::window::{Window, WindowBuilder};
 
-pub struct C2DWindowBuilder {}
+struct Resolution {
+    x: i32,
+    y: i32,
+}
+
+pub struct C2DWindowBuilder {
+    resolution: Resolution,
+    window_title: String,
+    window_decorations: bool,
+}
