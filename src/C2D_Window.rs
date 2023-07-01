843a317b54118fc18d58e6710bf37a8eb685b871
hinken <phnome@protonmail.com>
2023-07-02 01:11:18 +0200

    fixing defaults

--- src/C2D_Window.rs
+++ src/C2D_Window.rs
@@ -3,7 +3,7 @@ use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::window::{Window, WindowBuilder};
 
-#[dervive(Default)]
+#[derive(Default)]
 pub struct Resolution {
     pub x: i32,
     pub y: i32,
@@ -16,18 +16,14 @@ pub struct C2DWindowBuilder {
     pub window_decorations: bool,
 }
 
-impl Default for Resolution {
-    fn default() -> Self {
-        Resolution { x: 1920, y: 1080 }
+impl Resolution {
+    pub fn new() -> Self {
+        Default::default()
     }
 }
 
-impl Default for C2DWindowBuilder {
-    fn default() -> Self {
-        C2DWindowBuilder {
-            resolution: Resolution::default(),
-            window_title: String::from("Corrosion 2D"),
-            window_decorations: true,
-        }
+impl C2DWindowBuilder {
+    pub fn new() -> Self {
+        Default::default()
     }
 }
